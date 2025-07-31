68883b9c98048b550ab84a1b127c46388bc2c759
himanshuddgit <himanshudata4@gmail.com>
2025-07-31 18:36:05 +0530

    update

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,12 @@
+import VideoUpload from '@/components/VideoUpload'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full h-screen bg-neutral-900 flex items-center justify-center'>
+        <VideoUpload />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/VideoUpload.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function VideoUpload() {
+  return (
+    <div className='w-[500px] h-[300px] bg-neutral-800/70 rounded-2xl border-2 border-neutral-300 border-dashed '>VideoUpload</div>
+  )
+}
